533d4428ae9e07282208c68d959fd370c1cd5f0a
Louise Pabor <paborlouise@gmail.com>
2024-07-05 19:33:52 +0100

    Filter tasks based on status and proirity in JS, and did some refectoring in JS

--- static/js/tasks.js
+++ static/js/tasks.js
@@ -1,84 +1,120 @@
-const tasksContainer = document.querySelector("#task-grid-container");
 const loader = document.querySelector(".loader-container");
 const apiError = document.getElementById("fetchError");
-
+const taskContainer = document.querySelector(".tasks-container");
+const apiURL = 'http://127.0.0.1:8000/api-task-list/';
 
 // hide & show loader
 function showLoader() {
-    loader.classList.add("d-block");
+    loader.classList.add("flex");
+    loader.classList.remove("d-none");
 }
 function hideLoader() {
+    loader.classList.remove("flex");
     loader.classList.add("d-none");
 }
 
-// Getting Tasklist from an API
-function taskList() {
-    const taskContainer = document.querySelector(".tasks-container");
-    const taskListUrl = 'http://127.0.0.1:8000/api-task-list/';
+// Displaying fetched or filtered Tasks in the DOM
+const htmlFunc = (tasks) => {
+    const tasksContainer = document.getElementById("task-grid-container");
+    tasksContainer.innerHTML = "";
 
-    fetch(taskListUrl)
-        .then(res => {
-            if (!res.ok) {
-                throw new Error('Network response was not OK');
-            }
-            return res.json()
-        })
-        .then(data => {
-            console.log(data);
-            setTimeout(() => {
-                taskContainer.classList.remove("d-none");
-                hideLoader();
-                const tasks = data;
-    
-                for (const taskItem in tasks) {
-                    const taskDiv = `
-                    <div id="task-list-wrapper-${tasks[taskItem].pk}">
-                    <div class="grid align-ctr three-containers-grid priority-time-category">
-                        <div class="priority">${tasks[taskItem].priority}</div>
-                        <div class="flex-ctr justify-ctr time">
-                            <img src="static/img/icons/icons8-time-50.png" alt="" srcset="">
-                            ${new Date(tasks[taskItem].timestamp).toLocaleTimeString()}
-                        </div>
-                        <div class="category">${tasks[taskItem].category}</div>
+    tasks.forEach(task => {
+        const tasksDiv = document.createElement('div');
+        tasksDiv.setAttribute('id',task.id);
+
+        tasksDiv.innerHTML = `
+            <div class="grid align-ctr three-containers-grid priority-time-category">
+                <div class="priority">${task.priority}</div>
+                <div class="flex-ctr justify-ctr time">
+                    <img src="static/img/icons/icons8-time-50.png" alt="" srcset="">
+                    ${new Date(task.timestamp).toLocaleTimeString()}
+                </div>
+                <div class="category">${task.category}</div>
+            </div>
+            <div class="task-details">
+                <div class="flex flex-btw task-title">
+                    <a href="task-details/${task.id}/">${task.title}</a>
+                    <img src="static/img/icons/icons8-menu-vertical-50.png" alt="" srcset="">
+                </div>
+                <p class="task-desc">
+                    ${task.description}
+                </p>
+                <div class="flex-ctr task-date">
+                    <img src="static/img/icons/icons8-calender-64.png" alt="" srcset="">
+                    ${new Date(task.due_date).toLocaleDateString()}
+                </div>
+                <div class="flex flex-btw users-icons">
+                    <div class="relative flex profile-pix">
+                        <img src="static/img/profile-pic.jpg" alt="" srcset="" class="cover circle">
+                        <img src="static/img/profile-pic.jpg" alt="" srcset="" class="cover circle">
+                        <img src="static/img/profile-pic.jpg" alt="" srcset="" class="cover circle">
                     </div>
-                    <div class="task-details">
-                        <div class="flex flex-btw task-title">
-                            <a href="task-details/${tasks[taskItem].id}/">${tasks[taskItem].title}</a>
-                            <img src="static/img/icons/icons8-menu-vertical-50.png" alt="" srcset="">
-                        </div>
-                        <p class="task-desc">
-                            ${tasks[taskItem].description}
-                        </p>
-                        <div class="flex-ctr task-date">
-                            <img src="static/img/icons/icons8-calender-64.png" alt="" srcset="">
-                            ${new Date(tasks[taskItem].due_date).toLocaleDateString()}
-                        </div>
-                        <div class="flex flex-btw users-icons">
-                            <div class="relative flex profile-pix">
-                                <img src="static/img/profile-pic.jpg" alt="" srcset="" class="cover circle">
-                                <img src="static/img/profile-pic.jpg" alt="" srcset="" class="cover circle">
-                                <img src="static/img/profile-pic.jpg" alt="" srcset="" class="cover circle">
-                            </div>
-                            <div class="flex-ctr icons-div">
-                                <img src="static/img/icons/icons8-eye-50.png" alt="" srcset="">
-                                <img src="static/img/icons/icons8-trash-can-100.png" alt="" srcset="">
-                                <a href="task-update/${tasks[taskItem].id}/" class="edit-task">
-                                    <img src="static/img/icons/icons8-edit-32.png" alt="" srcset="">
-                                </a>
-                            </div>
-                        </div>
+                    <div class="flex-ctr icons-div">
+                        <img src="static/img/icons/icons8-eye-50.png" alt="" srcset="">
+                        <img src="static/img/icons/icons8-trash-can-100.png" alt="" srcset="">
+                        <a href="task-update/${task.id}/" class="edit-task">
+                            <img src="static/img/icons/icons8-edit-32.png" alt="" srcset="">
+                        </a>
                     </div>
                 </div>
-                    `
-                tasksContainer.innerHTML += taskDiv;
-                };
-            }, 1500);
-        })
-        .catch(error => {
-            hideLoader();
-            apiError.innerText = error.message;
-        })
+            </div>
+        `;
+    tasksContainer.appendChild(tasksDiv)
+    });
 }
 
+// Fecting from API
+async function fetchTasks() {
+    const response = await fetch(apiURL)
+    if (!response.ok) {
+        throw new Error('Network response was not OK');
+    }
+    const data =  await response.json();
+    return data;
+}
 
-taskList();
+// Fetching Tasks
+async function taskLists() {
+    const data = await fetchTasks()
+    .then((tasks) => {
+        setTimeout(() => {
+            taskContainer.classList.remove("d-none");
+            hideLoader();
+            htmlFunc(tasks);
+            console.log("------Fetching all Task lists---------");
+        }, 500);
+    })
+    .catch(error => {
+        hideLoader();
+        apiError.innerText = error.message;
+    });
+}
+
+// Filter tasks based on proirity and status
+async function filterTasks(event) {   
+    showLoader();
+    taskContainer.classList.add("d-none");
+    const input = event.target.value;
+    const data = await fetchTasks()
+    .then((tasks) => {
+        const flteredtasks = tasks.filter(task => task.status == input || task.priority == input);
+        console.log(flteredtasks);
+        setTimeout(() => {
+            taskContainer.classList.remove("d-none");
+            hideLoader();
+            htmlFunc(flteredtasks)
+        }, 500);
+    })
+    .catch(error => {
+        hideLoader();
+        apiError.innerText = error.message;
+    });
+    
+}
+// get sorted tasks
+function sortTasks(event) {   
+    console.log(event.target.value);
+    // fetchTasks();
+}
+
+window.onload = taskLists;
